fb3e09f7a8743b524023f4a9a469942d9231d228
stm0528 <sinhasatyam2805@gmail.com>
2026-02-05 18:18:28 +0530

    correct code for this program

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
-print("hey")
-a=4
-b=5
-print(a+b)
-print(b-a)
-c=34
-g=78
-sat=c*g
-print("this is the sum of" ,sat)
-"this is just a demo thing"
+"""Demo arithmetic operations."""
+
+print("Hey")
+
+a = 4
+b = 5
+
+print(a + b)
+print(b - a)
+
+c = 34
+g = 78
+result = c * g
+
+print("This is the product:", result)
+print("This is just a demo thing")
+
+
